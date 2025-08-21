7c285e6c3bb32fbf8984d2cb819ab1f39d167002
brunooliveiradsv <brunooliveiradsv@gmail.com>
2025-08-21 15:32:18 -0300

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Adicione esta linha:
+  base: '/brunoods/', // Use o nome do seu repositório aqui!
+
   plugins: [react()],
-  // ADICIONE ESTA LINHA:
-  base: '/brunoods/', 
 })
